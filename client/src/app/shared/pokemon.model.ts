53cf522822077ee609bd27454514736eb4402238
Johannes Andersen <johanes1@online.no>
2017-10-22 09:56:14 +0200

    Added table

--- client/src/app/shared/pokemon.model.ts
+++ client/src/app/shared/pokemon.model.ts
@@ -1,4 +1,14 @@
 export class Pokemon {
   number: number;
   name: string;
+  primaryType: string;
+  secondaryType: string;
+  health: number;
+  attack: number;
+  defence: number;
+  specialAttack: number;
+  specialDefence: number;
+  speed: number;
+  generation: string;
+  legendary: boolean;
 }
