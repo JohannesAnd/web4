9406d462d44a7eb124e04f6f0b8b9fc1ab39fcdb
Simon <smholsen@stud.ntnu.no>
2017-10-24 01:24:03 +0200

    Listview for pokemons. Also updated package.json.

--- client/src/app/pokemons/pokemons.component.ts
+++ client/src/app/pokemons/pokemons.component.ts
@@ -21,9 +21,26 @@ export class PokemonsComponent implements OnInit {
   ngOnInit(): void {
     this.pokemons$ = this.pokemonsService.pokemons$;
 
-    Observable.fromEvent(
-      this.getPokemons._elementRef.nativeElement,
-      'click'
-    ).subscribe(e => this.pokemonsService.getPokemons());
+    this.pokemonsService.getPokemons();
   }
+
+
+    shadeColor2(color, percent) {
+      var f=parseInt(color.slice(1),16),t=percent<0?0:255,p=percent<0?percent*-1:percent,R=f>>16,G=f>>8&0x00FF,B=f&0x0000FF;
+      return "#"+(0x1000000+(Math.round((t-R)*p)+R)*0x10000+(Math.round((t-G)*p)+G)*0x100+(Math.round((t-B)*p)+B)).toString(16).slice(1);
+    }
+
+    capitalizeFirstLetter(string) {
+      return string.charAt(0).toUpperCase() + string.slice(1);
+    }
+
+    getImageSource(unPaddedNumber){
+    let paddedNumber = "001";
+        if (unPaddedNumber <= 999) {
+          paddedNumber = ("000" + unPaddedNumber).slice(-3);
+        } else {
+          paddedNumber = unPaddedNumber;
+        }
+        return 'https://assets.pokemon.com/assets/cms2/img/pokedex/full/' + paddedNumber + '.png';
+    }
 }
